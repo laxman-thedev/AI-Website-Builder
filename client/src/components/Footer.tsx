7de5bf2a55b3798ddfbdf9a23eefa7d21bf51243
laxman-goud <codewithlaxman92@gmail.com>
2026-02-06 19:02:52 +0530

    feat(client): add Footer component with copyright information

---
+++ client/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <div className="text-center py-4 text-gray-400 text-sm border-t border-gray-800 mt-24">
+            <p>Copyright © 2026 AI Website Builder - <a href="https://laxman-thedev.me" target="_blank" className="underline" >Laxman</a></p>
+        </div>
+    )
+}
+
+export default Footer
