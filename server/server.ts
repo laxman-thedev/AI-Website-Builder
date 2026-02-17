8030fb98bef8fd5d5762823f62ec04c6788f35bf
laxman-goud <codewithlaxman92@gmail.com>
2026-02-17 19:11:04 +0530

    feat(server): integrate better-auth for user authentication handling

--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,21 @@
 import "dotenv/config";
 import express, { Request, Response } from 'express';
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
 const corsOptions = {
     origin: process.env.TRUSTED_ORIGINS?.split(",") || [], // Allow requests from this origin
-    Credentials: true, // Allow cookies to be sent with requests
+    credentials: true, // Allow cookies to be sent with requests
 }
 
 // Middleware
 app.use(cors(corsOptions))
+
+app.all('/api/auth/{*any}', toNodeHandler(auth));
+
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
