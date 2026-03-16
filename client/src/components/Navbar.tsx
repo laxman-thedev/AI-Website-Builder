0a82eab7d5a03469d7164a6df7d9c8eeecd55500
laxman-goud <codewithlaxman92@gmail.com>
2026-03-16 21:39:37 +0530

    feat: integrate credits display and fetch functionality in Navbar component

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,34 @@
-import { useState } from 'react'
+/* eslint-disable react-hooks/set-state-in-effect */
+import { useEffect, useState } from 'react'
 import { assets } from '../assets/assets';
 import { Link, useNavigate } from 'react-router-dom';
 import { authClient } from '@/lib/auth-client';
 import { UserButton } from '@daveyplate/better-auth-ui'
+import api from '@/configs/axios';
+import { toast } from 'sonner';
 
 const Navbar = () => {
 
     const [menuOpen, setMenuOpen] = useState(false);
     const navigate = useNavigate()
+    const [credits, setCredits] = useState(0)
 
     const { data: session } = authClient.useSession()
+
+    const getCredits = async() => {
+        try {
+            const {data} = await api.get('api/user/credits')
+            setCredits(data.credits)
+        } catch (error: any) {
+            toast.error(error?.response?.data?.message || 'Failed to fetch credits')
+        }
+    }
+
+    useEffect(() => {
+        if (session?.user) {
+            getCredits()
+        }
+    }, [session?.user])
 
 
     return (
@@ -32,7 +51,12 @@ const Navbar = () => {
                             Get started
                         </button>
                     ) : (
-                        <UserButton size="icon" />
+                        <>
+                            <button className='bg-white/10 px-5 py-1.5 text-xs sm:text-sm border text-gray-200 rounded-full'>
+                                Credits : <span className='text-indigo-300'>{credits}</span>
+                            </button>
+                            <UserButton size="icon" />
+                        </>
                     )
                     }
                     <button id="open-menu" className="md:hidden active:scale-90 transition" onClick={() => setMenuOpen(true)} >
